e3aa0592ae0f614721e6fa2da680762dbe2af269
DESKTOP-V0H394L\User <sebastiandevus@gmail.com>
2022-11-10 21:16:38 -0300

    Refatora para uso do event DOMContentLoader; Melhora documentação

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-let campoMedida = document.getElementById("medidaInserida");
+// Cria variáveis vazias para os campos necessários
+let campoMedida = "";
+let exibeMedidaCm = "";
+let exibeMedidaMetro = "";
+let exibeMedidaKm = "";
+let exibeMedidas = "";
 
-campoMedida.addEventListener("keydown", event => {
-    if (event.key == "Enter") {
-        event.preventDefault();
+// Executa setup após o documento estar carregado
+document.addEventListener("DOMContentLoaded", setup);
 
-        pegaExibeMedidas();
-    }
-});
+function setup() {
+    // Pega os campos necessários e armazena nas variáveis
+    campoMedida = document.getElementById("medidaInserida");
+    exibeMedidaCm = document.getElementById("exibeMedidaCm");
+    exibeMedidaMetro = document.getElementById("exibeMedidaMetro");
+    exibeMedidaKm = document.getElementById("exibeMedidaKm");
+    exibeMedidas = document.getElementById("exibeMedidas");
 
-campoMedida.addEventListener("change", event => {
-    pegaExibeMedidas();
-});
+    // Caso aperte enter, evite ação padrão e executa calculaExibeMedidas
+    campoMedida.addEventListener("keydown", (evt) => {
+        if (evt.key == "Enter") {
+            evt.preventDefault();
 
-function pegaExibeMedidas() {
-    const medidaEmMetros = Number(event.target.value)
+            calculaExibeMedidas();
+        }
+    });
+
+    // Em caso de mudança, executa calculaExibeMedidas
+    campoMedida.addEventListener("change", calculaExibeMedidas);
+}
+
+// Pega e exibe as medidas
+function calculaExibeMedidas() {
+    // Converte medidas
+    const medidaEmMetros = Number(campoMedida.value)
     const medidaEmCm = medidaEmMetros * 100;
     const medidaEmKm = medidaEmMetros / 1000;
-    
-    const exibeMedidaCm = document.getElementById("exibeMedidaCm");
-    const exibeMedidaMetro = document.getElementById("exibeMedidaMetro");
-    const exibeMedidaKm = document.getElementById("exibeMedidaKm");
-    
-    const exibeMedidas = document.getElementById("exibeMedidas");
-    exibeMedidas.hidden = 0;
 
-    exibeMedidaCm.innerHTML = `${medidaEmCm} cm`;
-    exibeMedidaMetro.innerHTML = `${medidaEmMetros} m`;
-    exibeMedidaKm.innerHTML = `${medidaEmKm} km`;    
+    // Adiciona texto aos campos de exibição
+    exibeMedidaCm.innerText = `${medidaEmCm} cm`;
+    exibeMedidaMetro.innerText = `${medidaEmMetros} m`;
+    exibeMedidaKm.innerText = `${medidaEmKm} km`;
+
+    // Altera a propriedade hidden da div parente dos campos de exibição 
+    exibeMedidas.hidden = 0;
 }
